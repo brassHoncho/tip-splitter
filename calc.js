1bd1d1c1646a8bc05ce8e15c48d83574f2334dff
brassHoncho <brasshoncho@gmail.com>
2026-06-23 09:38:40 -0400

    Refactor: extract total into a named variable

--- calc.js
+++ calc.js
@@ -2,7 +2,8 @@
 // Input: bill amount, tip percentage, number of people.
 // Output: how much each person pays (a plain number).
 function tipPerPerson(bill, tipPercent, people) {
-  return (bill * (1 + tipPercent / 100)) / people;
+  const total = bill * (1 + tipPercent / 100);
+  return total / people;
 }
 
 // Make it available to Node's test runner (CommonJS) when running tests.

2af293aa331d1012b4d48efc74d68d70157c9502
brassHoncho <brasshoncho@gmail.com>
2026-06-23 09:39:47 -0400

    Fix: guard against zero people; add test

--- calc.js
+++ calc.js
@@ -2,7 +2,7 @@
 // Input: bill amount, tip percentage, number of people.
 // Output: how much each person pays (a plain number).
 function tipPerPerson(bill, tipPercent, people) {
-  return (bill * (1 + tipPercent / 100)) / people;
+  return (bill * (1 + tipPercent / 100)) / Math.max(people, 1);
 }
 
 // Make it available to Node's test runner (CommonJS) when running tests.
